f2706a640b5e9095a99f89b40651b42c50c3cc43
EzequielAlvizoFTG <ealvizo@forbestravelguide.com>
2025-05-02 11:11:27 -0500

    Cambios desde VisualStudioCode

--- practica2/codigo.js
+++ practica2/codigo.js
@@ -3,25 +3,22 @@ let listaAsistencias = [];
 
 document.write("hola");
 
-function lista(dia){
-	document.write(dia + "<br>");
-	let asistencia = prompt("LLego a clase?");
-	if (asistencia == "p" || asistencia == "P") {
-		return 1
-	}
-	else {
-		return 0
-	}
+function lista(dia) {
+  document.write(dia + "<br>");
+  let asistencia = prompt("LLego a clase?");
+  if (asistencia == "p" || asistencia == "P") {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
-for (let i = 0; i < alumnos; i++){
-	listaAsistencias.push([]);
+for (let i = 0; i < alumnos; i++) {
+  listaAsistencias.push([]);
 }
 
-for (let dia = 0; dia < 30; dia++){
-	for (let alumno = 0; alumno < alumnos; alumno++){
-		listaAsistencias[alumno].push(lista("dia " + (dia+1)))
-	}
+for (let dia = 0; dia < 30; dia++) {
+  for (let alumno = 0; alumno < alumnos; alumno++) {
+    listaAsistencias[alumno].push(lista("dia " + (dia + 1)));
+  }
 }
-
-console.log(listaAsistencias)
